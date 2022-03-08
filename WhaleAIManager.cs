9d9808a9d6c7b91b0ebc965dd5b4c1d81572564e
MoDPOwer <santiagoantonio52@gmail.com>
2022-03-07 22:25:43 -0300

    Add files via upload

---
+++ WhaleAIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhaleAIManager : MonoBehaviour {
+
+    private WhaleMovementController whaleMovementController;
+
+	// Use this for initialization
+	void Start () {
+        whaleMovementController = GetComponent<WhaleMovementController>();		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        whaleMovementController.Move();
+		
+	}
+}
