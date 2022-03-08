ab1cb630f78603f0b43a86715105adca599f020b
MoDPOwer <santiagoantonio52@gmail.com>
2022-03-07 22:18:17 -0300

    Add files via upload

---
+++ DrinkBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+public class DrinkBehavior : MonoBehaviour {
+	public static float DrinkPlayer;
+	public float seconds;
+	public float secondsh;
+	public Texture Drink;
+	public int FullDrink = 100;
+	void Start (){
+		DrinkPlayer = FullDrink;
+	}
+	void Update (){
+		seconds +=Time.deltaTime;
+		if (seconds >= 5) {
+			DrinkPlayer -= 1;
+			seconds = 0;
+
+		if (DrinkPlayer >= FullDrink) {
+			DrinkPlayer = FullDrink;
+		} else if (DrinkPlayer <= 0) {
+			DrinkPlayer = 0;
+		}
+	}
+	if (DrinkPlayer <= 0) {
+		secondsh +=Time.deltaTime;
+		if (secondsh >= 1) {
+			HealthBehavior.HealthPlayer --;
+			secondsh = 0;
+		}
+	}
+}
+	void OnGUI (){
+		GUI.DrawTexture (new Rect (Screen.width / 30, Screen.height / 16, Screen.width / 7f/FullDrink*DrinkPlayer, Screen.height / 70), Drink);
+
+	}
+}
